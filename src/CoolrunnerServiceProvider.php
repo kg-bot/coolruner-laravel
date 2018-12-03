64a691d2b91a2556dec6a86cd11aab1d0f0dd137
Danijel <dadodju@gmail.com>
2018-12-03 15:07:45 +0100

    Updated service provider

--- src/CoolrunnerServiceProvider.php
+++ src/CoolrunnerServiceProvider.php
@@ -2,7 +2,27 @@
 
 namespace Coolrunner;
 
-class CoolrunnerServiceProvider
+use Carbon\Laravel\ServiceProvider;
+
+class CoolrunnerServiceProvider extends ServiceProvider
 {
-    
+    public function boot()
+    {
+        $configPath = __DIR__.'/config/coolrunner.php';
+
+        if (function_exists('config_path')) {
+
+            $publishPath = config_path('coolrunner.php');
+
+        } else {
+
+            $publishPath = base_path('config/coolrunner.php');
+
+        }
+
+        $this->publishes([$configPath => $publishPath], 'config');
+    }
+    public function register()
+    {
+    }
 }
